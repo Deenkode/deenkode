36ae5fe57ff87bec3bd11129f95928d1a5a548d4
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-06-19 02:30:27 +0600

    Add GitHub Actions deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,6 @@
-import { defineConfig } from 'vite';import tailwindcss from '@tailwindcss/vite';export default defineConfig({  plugins: [    tailwindcss(),  ],})
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({ 
+      base: '/',
+    plugins: [tailwindcss()] });
+// vite.config.js
